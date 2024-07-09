d3a2ce982794d694a872ff42ee6191e45c407a97
sachintha <sachintha.s.gunawardhana@gmail.com>
2024-07-09 15:39:20 +0530

    fix Home page height issue

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
         <Navbar/>
-        <div className=' bg-slate-500 h-[100vh]  lg:flex lg:pt-0 pt-16 lg:justify-between flex lg:flex-row flex-col  justify-center lg:justify-between  '>
+        <div className=' bg-slate-500 lg:h-full h-[100vh]  lg:flex lg:pt-0 pt-16 lg:justify-between flex lg:flex-row flex-col  justify-center lg:justify-between  '>
           <div className='order-2 lg:order-1 '>
             <h1 className='font-mono text-3xl font-extrabold text-center lg:text-left lg:text-9xl'>Hello! <br /> I AM <br /> SACHINTHA</h1>
             <h3 className='font-bold text-1xl lg:text-3xl'>You can get a better idea about me from this site.</h3>
